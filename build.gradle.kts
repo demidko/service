e5d810d1883015f8666f94a3ea033a34059d2cf5
Daniil Demidko <demidko@farpost.com>
2021-04-12 22:11:53 +1000

    Упростил шаблон

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,8 @@ plugins {
 
 dependencies {
   implementation("ch.qos.logback:logback-classic:1.3.0-alpha5")
-  implementation("com.github.ajalt.clikt:clikt:3.1.0")
   implementation("io.ktor:ktor-server-netty:1.5.2")
-  implementation("io.ktor:ktor-client-cio:1.5.2")
+  testImplementation("io.ktor:ktor-client-cio:1.5.2")
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.0-M1")
   testImplementation("com.natpryce:hamkrest:1.8.0.1")
   testImplementation("io.mockk:mockk:1.10.6")
--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,6 +1,3 @@
-import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.arguments.multiple
 import io.ktor.application.*
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.response.*
@@ -8,16 +5,8 @@ import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) = application().main(args)
-
-fun application() = object : CliktCommand("Application description here") {
-
-  val opt by argument().multiple()
-
-  override fun run() {
-    echo(opt)
-    localhostServer().start(true)
-  }
+fun main(args: Array<String>) {
+  localhostServer().start(true)
 }
 
 fun localhostServer() = embeddedServer(Netty) {
