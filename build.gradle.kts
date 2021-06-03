3296c1c91854db4c6394a1d2f2837f9fe23c54b1
Daniil Demidko <demidko@farpost.com>
2021-06-04 01:11:05 +1000

    jdk16

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,31 +2,30 @@ repositories {
   mavenCentral()
   maven("https://jitpack.io")
 }
-
 plugins {
-  kotlin("jvm") version "1.4.30"
+  kotlin("jvm") version "1.5.10"
   id("com.github.johnrengelman.shadow") version "6.1.0"
 }
-
 dependencies {
+  implementation("io.ktor:ktor-server-netty:1.6.0")
+  implementation("org.redisson:redisson:3.15.5")
+  implementation("co.touchlab:stately-isolate-jvm:1.1.7-a1")
   implementation("ch.qos.logback:logback-classic:1.3.0-alpha5")
-  implementation("io.ktor:ktor-server-netty:1.5.3")
-  testImplementation("io.ktor:ktor-client-cio:1.5.2")
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.0-M1")
   testImplementation("com.natpryce:hamkrest:1.8.0.1")
-  testImplementation("io.mockk:mockk:1.10.6")
+  testImplementation("io.mockk:mockk:1.11.0")
 }
-
+tasks.compileKotlin {
+  kotlinOptions.jvmTarget = "16"
+  kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.time.ExperimentalTime"
+}
+tasks.compileTestKotlin {
+  kotlinOptions.jvmTarget = "16"
+  kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.time.ExperimentalTime"
+}
 tasks.test {
   useJUnitPlatform()
 }
-
-tasks.compileKotlin {
-  kotlinOptions.jvmTarget = "15"
-}
-
 tasks.jar {
-  manifest {
-    attributes("Main-Class" to "AppKt")
-  }
+  manifest.attributes("Main-Class" to "AppKt")
 }
