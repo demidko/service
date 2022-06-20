5797af52169165d1fd35a75644b9b28f9145176f
Daniel Demidko <demidko@farpost.com>
2022-06-20 12:10:03 +0500

    routing import

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,7 +1,4 @@
-import io.ktor.application.*
 import io.ktor.http.HttpStatusCode.Companion.OK
-import io.ktor.response.*
-import io.ktor.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
