64438d7202a500fc4ef337b169d214efcfe54d81
Daniel Demidko <demidko@farpost.com>
2022-06-20 12:09:53 +0500

    routing import

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -2,8 +2,11 @@ import io.ktor.application.*
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main(args: Array<String>) {
   localhostServer().start(true)
