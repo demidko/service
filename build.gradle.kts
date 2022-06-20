2c939e266d9a3677eabc463c661ff74896918db5
demidko <demidko@farpost.com>
2022-06-20 11:56:55 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,11 @@ dependencies {
   testImplementation("io.mockk:mockk:1.12.4")
 }
 tasks.compileKotlin {
-  kotlinOptions.jvmTarget = "17"
+  kotlinOptions.jvmTarget = "18"
   kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.time.ExperimentalTime"
 }
 tasks.compileTestKotlin {
-  kotlinOptions.jvmTarget = "17"
+  kotlinOptions.jvmTarget = "18"
   kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.time.ExperimentalTime"
 }
 tasks.test {
