bb3bf501f16a869c8cac59a4e5822a17f1167cbc
Daniel Demidko <demidko@farpost.com>
2022-11-11 06:48:47 +0500

    Gradle config simplified

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 repositories {
   mavenCentral()
   maven("https://jitpack.io")
@@ -15,11 +17,7 @@ dependencies {
   testImplementation("com.google.truth:truth:1.1.3")
   testImplementation("io.mockk:mockk:1.13.2")
 }
-tasks.compileKotlin {
-  kotlinOptions.jvmTarget = "17"
-  kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.time.ExperimentalTime"
-}
-tasks.compileTestKotlin {
+tasks..withType<KotlinCompile> {
   kotlinOptions.jvmTarget = "17"
   kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.time.ExperimentalTime"
 }
